7d392b22e3065df90cd2fe7dc1cff8fd77c31798
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-09-04 13:24:40 -0400

    Create TextAnalyzer.java

---
+++ Lab02/TextAnalyzer.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+public class TextAnalyzer {
+    private int lineCount;
+    private int wordCount;
+    private int[] frequencies = new int[26];
+
+
+    public void analyzeText(String fileName) {
+        
+    }
+
+
+
+
+
+
+
+    public int[] getFrequencies{
+        return frequencies;
+    }
+    public int getLineCount() {
+        return lineCount;
+    }
+    public int getWordCount() {
+        return wordCount;
+    }
+
+}
