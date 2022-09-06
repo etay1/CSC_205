13f43e787acc0617e07676df7d6879a44c6aafe2
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-09-06 00:25:26 -0400

    Update TextAnalyzer.java

--- Lab02/TextAnalyzer.java
+++ Lab02/TextAnalyzer.java
@@ -6,31 +6,57 @@ public class TextAnalyzer {
     private int lineCount;
     private int wordCount;
     private int[] frequencies = new int[26];
-    
+
     public TextAnalyzer() {
         this.lineCount = 0;
         this.wordCount = 0;
     }
     public void analyzeText(String fileName) {
+        //here we can initialize the br so we can use the io exception
         BufferedReader br = null;
-        String line;
+        String line = "";
+
         try {
+            /*to save lines, instead of creating a fr for file reader
+            we can just use the new FileReader and the analyzeText parameter
+            to read into our buffered reader
+             */
             br = new BufferedReader(new FileReader(fileName));
-            while ((line = br.readLine()) != null) {
+            /*
+            tried to do a do loop but it failed so here's this
+            i want a do loop...
+            -assign our initialized line to read each line of the file (1 at a time)
+            -then if that line isn't null repeat the block
+            * */
+            while ((line = br.readLine()) != null) { //while
+                //simple increment to the lineCount every loop
                 lineCount++;
-                //then we'll use split to get the word counts based on spaces and the length
-                //we'll also be calling the methods required to get the frequency of chars in this try catch block
+                //split the text by spaces
                 String[] split = line.split(" ");
+                // wordCount should be equal to the number of spaces in-between words
                 wordCount += split.length;
+
+                /*I think this is where we will call the frequency method
+                * and probably a charMatch method or maybe combine the two somehow
+                *Sample Output for the HumptyDumpty.txt
+                Enter the name of the file:
+                HumptyDumpty.txt
+                Number of Lines: 8
+                Number of Words: 54
+                Letter Counts
+                *--so now we just have to make the frequency method and post the array of indices [0-25]
+                * ASCII?
+                * */
             }
         }   catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     public int getLineCount() {
         return lineCount;
     }
+
     public int getWordCount() {
         return wordCount;
     }
