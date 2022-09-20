81b37a774baeb8bb0c3347983bd932cae4167038
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-09-20 13:51:02 -0400

    Add files via upload

---
+++ Lab04/IntSortSearchTester.java
@@ -0,0 +1,20 @@
+
+public class IntSortSearchTester {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void linearSearchTester() {
+		
+	}
+	
+	public static void binarySearchTester() {
+		
+	}
+	
+	public static void sortTester() {
+		
+	}
+
+}
